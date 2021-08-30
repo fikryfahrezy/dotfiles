b8fa578bfb529039088e022033334345ad2e8594
Fikry Fahrezy <ffahrezi69@gmail.com>
2021-08-30 10:24:02 +0700

    add hard link script

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ PWD=`pwd`
 
 echo "Running $FN in $FWD from $PWD"
 
+ln $FWD/scripts/ldf.sh $FWD/arch/ldf.sh 
+ln $FWD/scripts/ldf.sh $FWD/bash/ldf.sh
+ln $FWD/scripts/ldf.sh $FWD/nvim/ldf.sh
+ln $FWD/scripts/ldf.sh $FWD/zsh/ldf.sh
+
 # https://unix.stackexchange.com/a/210656
 if [ "$(grep -Ei 'arch|manjaro' /etc/*release)" ]; then
   echo "Hi Arch Based user"
@@ -16,10 +21,10 @@ else
   echo "Hi, who are you?"
 fi
 
-# $FWD/scripts/nodejs.sh
-# $FWD/scripts/golang.sh
-# $FWD/nvim/install.sh
-# $FWD/bash/install.sh
-# $FWD/zsh/install.sh
+$FWD/scripts/nodejs.sh
+$FWD/scripts/golang.sh
+$FWD/nvim/install.sh
+$FWD/bash/install.sh
+$FWD/zsh/install.sh
 
 echo "Done runninig $FN in $FWD from $PWD"
