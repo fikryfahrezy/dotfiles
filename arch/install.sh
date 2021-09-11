8df503982d253da9c46e418d697f493cf7697379
Fikry Fahrezy <ffahrezi69@gmail.com>
2021-09-11 15:35:43 +0700

    update arch folder

--- arch/install.sh
+++ arch/install.sh
@@ -9,5 +9,7 @@ CWD="$(pwd)/$FWD/"
 pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
 
 sudo $CWD/ldf.sh $CWD/install.hook /etc/pacman.d/hooks/install.hook
+sudo $CWD/ldf.sh $CWD/pkglist.txt /etc/pacman.d/hooks/pkglist.txt
+sudo $CWD/ldf.sh $CWD/foreignpkglist.txt /etc/pacman.d/hooks/foreignpkglist.txt
 
 echo "Done running arch/install.sh"
