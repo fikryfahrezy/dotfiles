28d186b49868d195e6d86d447aa1d093b6d856cc
Fikry Fahrezy <ffahrezi69@gmail.com>
2021-09-15 22:08:56 +0700

    move to SpaceVim

--- nvim/install.sh
+++ nvim/install.sh
@@ -12,10 +12,10 @@ chmod u+x $FWD/nvim.appimage
 $FWD/nvim.appimage --appimage-extract
 
 sudo mv squashfs-root /
-
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 $CWD/ldf.sh -s $CWD/init.vim $HOME/.config/nvim/init.vim
+$CWD/ldf.sh -s $CWD/main.vim $HOME/.config/nvim/main.vim
 
 rm -rf $FWD/nvim.appimage
 rm -rf $FWD/squashfs-root
@@ -26,7 +26,13 @@ nvim --version
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Install COC
+# link COC setting
 $CWD/ldf.sh -s $CWD/coc-settings.json $HOME/.config/nvim/coc-settings.json
+
+# Install SpaceVim
+curl -sLf https://spacevim.org/install.sh | bash
+
+$CWD/ldf.sh -s $CWD/init.toml $HOME/.SpaceVim.d/init.toml
+$CWD/ldf.sh -s $CWD/myspacevim.vim $HOME/.SpaceVim.d/autoload/myspacevim.vim
 
 echo "Done running nvim/install.sh"
--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -8,4 +8,19 @@ curl https://get.volta.sh | bash
 # install Node
 volta install node
 
+# install other tools via Volta
+volta install create-next-app
+volta install depcheck
+volta install eslint
+volta install http-server
+volta install importjs
+volta install js-beautify
+volta install neovim
+volta install npkill
+volta install pnpm
+volta install react-native
+volta install typescript
+volta install typescript-formatter
+volta install vercel
+
 echo "Done running scripts/nodejs.sh"
