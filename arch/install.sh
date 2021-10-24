800646b7e72b69054c42530f4a85c6ed12de9f8c
Fikry Fahrezy <ffahrezi69@gmail.com>
2021-10-24 20:28:32 +0700

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -6,7 +6,7 @@ FWD=`dirname "$0"`
 CWD="$(pwd)/$FWD/"
 
 # https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Install_packages_from_a_list
-pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort $CWD/pkglist.txt))
 
 sudo $CWD/ldf.sh $CWD/install.hook /etc/pacman.d/hooks/install.hook s
 sudo $CWD/ldf.sh $CWD/pkglist.txt /etc/pacman.d/hooks/pkglist.txt s
