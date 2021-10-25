6f2a8849952d00845898aa22521e127cdfe125b7
Fikry Fahrezy <ffahrezi69@gmail.com>
2021-10-25 13:54:59 +0700

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -34,6 +34,6 @@ docker run --name some-mysql -v "$HOME/Docker/mysql/mysql":/var/lib/mysql -v "$H
 docker run --name some-postgre -e PGDATA=/var/lib/postgresql/data/pgdata -v "$HOME/Docker/postgresql/data":/var/lib/postgresql/data -e POSTGRES_PASSWORD=postgres --network="postgre_net" -p 5432:5432 --restart=unless-stopped -d postgres:13.4
 docker run --name some-mongo -v "$HOME/Docker/mongo/data/db":/data/db -e MONGO_INITDB_ROOT_USERNAME=mongo -e MONGO_INITDB_ROOT_PASSWORD=mongo --network="mongo_net" -p 27017:27017 --restart=unless-stopped -d mongo:4.2
 docker run --name phpmyadmin --link some_mysql:db --network="mysql_net" -p 3307:80 -d phpmyadmin/phpmyadmin:5.1.1
-docker run -e "PGADMIN_DEFAULT_EMAIL=postgre@postgre.com" -e "PGADMIN_DEFAULT_PASSWORD=postgre" --network="postgre_net" -p 5431:80  -d dpage/pgadmin4:5
+docker run --name pgadmin -e "PGADMIN_DEFAULT_EMAIL=postgre@postgre.com" -e "PGADMIN_DEFAULT_PASSWORD=postgre" --network="postgre_net" -p 5431:80  -d dpage/pgadmin4:5
 
 echo "Done running docker/install.sh"
